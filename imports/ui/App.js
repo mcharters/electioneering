bfc667f8131639789d86a17376ae855a2c32adc0
Matthew Charters <matthew@thirdsectorpublishing.ca>
2018-08-07 20:54:57 -0400

    Add react router

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,33 +1,11 @@
-import React, { PureComponent } from 'react';
-import AddressList from './AddressList.js';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ChooseAddress from './ChooseAddress';
 
-// App component - represents the whole app
-class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      position: null,
-    };
-  }
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => { this.setState({ position }); },
-    );
-  }
-
-  render() {
-    const { position } = this.state;
-
-    if (position === null) {
-      return (
-        <h1>
-          {'Finding location'}
-        </h1>);
-    }
-
-    return <AddressList position={position} />;
-  }
-}
+const App = () => (
+  <Router>
+    <Route exact path="/" component={ChooseAddress} />
+  </Router>
+);
 
 export default App;
---
+++ imports/ui/ChooseAddress.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import AddressList from './AddressList.js';
+
+// App component - represents the whole app
+class ChooseAddress extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => { this.setState({ position }); },
+    );
+  }
+
+  render() {
+    const { position } = this.state;
+
+    if (position === null) {
+      return (
+        <h1>
+          {'Finding location'}
+        </h1>);
+    }
+
+    return <AddressList position={position} />;
+  }
+}
+
+export default ChooseAddress;
