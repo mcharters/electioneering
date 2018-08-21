9be7d7bd5909525d40a15489b5fac5109cafba35
Matthew Charters <matthew@thirdsectorpublishing.ca>
2018-08-20 22:06:23 -0400

    Add some libraries to help with easy forms

--- imports/api/people.js
+++ imports/api/people.js
@@ -1,5 +1,18 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
 
 const People = new Mongo.Collection('people', { idGeneration: 'MONGO' });
+People.schema = new SimpleSchema({
+  name: String,
+  email: SimpleSchema.RegEx.EmailWithTLD,
+  phone: SimpleSchema.RegEx.Phone,
+  status: { type: SimpleSchema.Integer, allowedValues: [1, 2, 3, 4] },
+  reiminder: Boolean,
+  newsletter: Boolean,
+  lawnSign: Boolean,
+  canvas: Boolean,
+  donate: Boolean,
+  notes: String,
+});
 
 export default People;
--- imports/ui/Person.js
+++ imports/ui/Person.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { AutoForm } from 'uniforms-bootstrap4';
 import Addresses from '../api/addresses';
 import People from '../api/people';
 
-const Person = ({ person, loading }) => {
+const Person = ({ person, loading, address }) => {
   if (loading) {
     return null;
   }
 
-  if (person === null) {
-    return 'New person';
-  }
-
-  return 'Edit person';
+  return (
+    <div>
+      <h1>
+        {`Add new person at ${address.address}`}
+      </h1>
+      <AutoForm schema={People.schema} />
+    </div>
+  );
 };
 
 export default withTracker(({ match }) => {
   const addressHandle = Meteor.subscribe('addresses.withId', match.params.addressId);
-  const address = Addresses.findOne(new Mongo.ObjectID(match.params.id));
+  const address = Addresses.findOne(new Mongo.ObjectID(match.params.addressId));
 
   let person = null;
-  if (match.params.personId !== 'new') {
+  if (match.params.personId === 'new') {
     person = {};
   }
 
